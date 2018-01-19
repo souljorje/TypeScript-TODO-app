0f5c5513572e3a54e73e961ac0acca27439cec80
Георгий Бухаров <gbu@crtweb.ru>
2018-01-19 17:11:47 +0500

    ts: types edits, css: some styles

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+;
 var TodoApp = /** @class */ (function () {
     function TodoApp(options) {
-        this.input = document.querySelector(options.input);
-        this.addButton = document.querySelector(options.addButton);
-        this.list = document.querySelector(options.list);
+        this.input = options.input;
+        this.addButton = options.addButton;
+        this.list = options.list;
         this.listItem = options.listItem;
         this.removeButton = options.removeButton;
-        this.createNewElement = this.createNewElement.bind(this);
         this.add = this.add.bind(this);
         this.remove = this.remove.bind(this);
         this.toggle = this.toggle.bind(this);
@@ -17,48 +17,48 @@ var TodoApp = /** @class */ (function () {
         }
         return el;
     };
-    TodoApp.prototype.init = function () {
+    TodoApp.prototype.bindEvents = function () {
         var _this = this;
-        this.addButton.addEventListener('click', this.add);
+        document.querySelector("." + this.addButton).addEventListener('click', this.add);
         document.body.addEventListener('click', function (e) { return _this.remove(e); });
         document.body.addEventListener('click', function (e) { return _this.toggle(e); });
     };
     TodoApp.prototype.add = function () {
-        var inputValue = this.input.value;
+        var inputValue = document.querySelector("." + this.input).value;
         if (inputValue.length) {
             var li = this.createNewElement('li', {
-                'class': this.listItem,
+                'class': this.listItem
             });
             var text = document.createTextNode(inputValue);
             var removeButton = this.createNewElement('span', {
-                'class': this.removeButton,
+                'class': this.removeButton
             });
             li.appendChild(text);
             li.appendChild(removeButton);
-            this.list.appendChild(li);
-            this.input.value = '';
+            document.querySelector("." + this.list).appendChild(li);
+            document.querySelector("." + this.input).value = '';
         }
         else {
             alert('Write something!');
         }
     };
     TodoApp.prototype.remove = function (e) {
-        if (e.target.classList.contains(this.removeButton)) {
-            e.target.closest('.' + this.listItem).style.display = 'none';
+        if (e.target.classList.contains((this.removeButton))) {
+            e.target.closest("." + this.listItem).style.display = 'none';
         }
     };
     TodoApp.prototype.toggle = function (e) {
         if (e.target.classList.contains(this.listItem)) {
-            e.target.classList.toggle(this.listItem + '_is_checked');
+            e.target.classList.toggle(this.listItem + "_is_checked");
         }
     };
     return TodoApp;
 }());
 var MyTodo = new TodoApp({
-    input: '.todo__input',
-    addButton: '.todo__button',
-    list: '.todo__list',
+    input: 'todo__input',
+    addButton: 'todo__button',
+    list: 'todo__list',
     listItem: 'todo__item',
-    removeButton: 'todo__remove',
+    removeButton: 'todo__remove'
 });
-MyTodo.init();
+MyTodo.bindEvents();
--- app.ts
+++ app.ts
@@ -1,73 +1,80 @@
-class TodoApp {
-    input: object;
-    addButton: object;
-    list: object;
-    listItem: string;
-    removeButton: string;
-    constructor(options) {
-        this.input = document.querySelector(options.input);
-        this.addButton = document.querySelector(options.addButton)
-        this.list = document.querySelector(options.list);
-        this.listItem = options.listItem;
-        this.removeButton = options.removeButton;
-        this.createNewElement = this.createNewElement.bind(this);
-        this.add = this.add.bind(this);
-        this.remove = this.remove.bind(this);
-        this.toggle = this.toggle.bind(this);
-    }
+interface TodoInterface {
+  input: string;
+  list: string;
+  listItem: string;
+  removeButton: string;
+};
 
-    private createNewElement(tag: string, attrs: object): object {
-        let el = document.createElement(tag);
-        for (let attr in attrs) {
-            el.setAttribute(attr, attrs[attr]);
-        }
-        return el;
-    }
+class TodoApp implements TodoInterface {
+  input: string;
+  addButton: string;
+  list: string;
+  listItem: string;
+  removeButton: string;
+  options: object;
+  constructor(options) {
+    this.input = options.input;
+    this.addButton = options.addButton;
+    this.list = options.list;
+    this.listItem = options.listItem;
+    this.removeButton = options.removeButton;
+    this.add = this.add.bind(this);
+    this.remove = this.remove.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
 
-    private init(): void {
-        this.addButton.addEventListener('click', this.add);
-        document.body.addEventListener('click', (e) => this.remove(e));
-        document.body.addEventListener('click', (e) => this.toggle(e));
+  private createNewElement(tag: string, attrs: object): HTMLElement {
+    const el = document.createElement(tag);
+    for (let attr in attrs) {
+      el.setAttribute(attr, attrs[attr]);
     }
+    return el;
+  }
 
-    public add(): void {
-        let inputValue = this.input.value;
-        if (inputValue.length) {
-            let li = this.createNewElement('li', {
-                'class': this.listItem,
-            });
-            let text = document.createTextNode(inputValue);
-            let removeButton = this.createNewElement('span', {
-                'class': this.removeButton,
-            })
-            li.appendChild(text);
-            li.appendChild(removeButton);
-            this.list.appendChild(li);
-            this.input.value = '';
-        } else {
-            alert('Write something!')
-        }
-    }
+  public bindEvents(): void {
+    document.querySelector(`.${this.addButton}`).addEventListener('click', this.add);
+    document.body.addEventListener('click', (e) => this.remove(e));
+    document.body.addEventListener('click', (e) => this.toggle(e));
+  }
 
-    public remove(e): void {
-        if (e.target.classList.contains(this.removeButton)) {
-            e.target.closest('.' + this.listItem).style.display = 'none';
-        }
+  public add(): void {
+    const inputValue = document.querySelector<HTMLInputElement>(`.${this.input}`).value;
+    if (inputValue.length) {
+      const li = this.createNewElement('li', {
+        'class': this.listItem,
+      });
+      const text = document.createTextNode(inputValue);
+      const removeButton = this.createNewElement('span', {
+        'class': this.removeButton,
+      })
+      li.appendChild(text);
+      li.appendChild(removeButton);
+      document.querySelector<HTMLUListElement>(`.${this.list}`).appendChild(li);
+      document.querySelector<HTMLInputElement>(`.${this.input}`).value = '';
+    } else {
+      alert('Write something!')
     }
+  }
 
-    public toggle(e): void {
-        if (e.target.classList.contains(this.listItem)) {
-            e.target.classList.toggle(this.listItem + '_is_checked')
-        }
+  public remove(e): void {
+    if (e.target.classList.contains((this.removeButton))) {
+      e.target.closest(`.${this.listItem}`).style.display = 'none';
     }
+  }
+
+  public toggle(e): void {
+    if (e.target.classList.contains(this.listItem)) {
+      e.target.classList.toggle(`${this.listItem}_is_checked`)
+    }
+  }
 }
 
 const MyTodo = new TodoApp({
-    input: '.todo__input',
-    addButton: '.todo__button',
-    list: '.todo__list',
-    listItem: 'todo__item',
-    removeButton: 'todo__remove',
+  input: 'todo__input',
+  addButton: 'todo__button',
+  list: 'todo__list',
+  listItem: 'todo__item',
+  removeButton: 'todo__remove',
 });
 
-MyTodo.init();
+MyTodo.bindEvents();
